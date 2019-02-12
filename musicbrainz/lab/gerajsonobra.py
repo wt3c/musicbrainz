d4b0e37f8229800c7e2e4d4e5e485b294a2f2c2f
Welington Carlos <wcarlos3@gmail.com>
2019-02-12 15:59:36 -0200

    testando...

--- musicbrainz/lab/gerajsonobra.py
+++ musicbrainz/lab/gerajsonobra.py
@@ -3,58 +3,109 @@ from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
 
+from IPython.display import display_html
 
 with open('titulares.json', 'r') as f:
     tits = json.load(f)
 
-# url 'https://musicbrainz.org/artist/42a636a0-dbe4-4d0c-abe2-1590fad9531b/works'
-url = 'https://musicbrainz.org/artist/'
+url = 'https://musicbrainz.org/artist/42a636a0-dbe4-4d0c-abe2-1590fad9531b/works'
+# url = 'https://musicbrainz.org/artist/'
 
-for gid in tits[:10]:
-    link = str(url + gid['gid'] + '/works')
-    # print(link)
-    response = get(link)
-    # print(response)
-    html_soup = BeautifulSoup(response.text, 'html.parser')
-    # print(html_soup)
+dfs = pd.read_html(url, header=0)
+for df in dfs:
+    print(df)
 
+#
+# for gid in tits[:10]:
+#     link = str(url + gid['gid'] + '/works')
+
+
+# print(link)
+# response = get(link)
+# # print(response)
+# html_soup = BeautifulSoup(response.text, 'html.parser')
+# # html_soup = BeautifulSoup(response.text, 'lxml')
+#
+# table = html_soup.find('table')
+# table_rows = html_soup.find_all('tr')
+#
+# for tr in table_rows:
+#     td = tr.find_all('td')
+#     row = [i.text for i in td]
+#     print(row)
+#
+#
+# # print(table)
+# # print('*'*50)
+
+# print(display_html(html_soup.table, raw=True))
+
+html_string = """
+    <table>
+      <tr>
+        <th>Programming Language</th>
+        <th>Creator</th> 
+        <th>Year</th>
+      </tr>
+      <tr>
+        <td>C</td>
+        <td>Dennis Ritchie</td> 
+        <td>1972</td>
+      </tr>
+      <tr>
+        <td>Python</td>
+        <td>Guido Van Rossum</td> 
+        <td>1989</td>
+      </tr>
+      <tr>
+        <td>Ruby</td>
+        <td>Yukihiro Matsumoto</td> 
+        <td>1995</td>
+      </tr>
+    </table>
     """
-            table_work class="tbl">
-            <thead>
-                <tr>
-                    <th> Work 'Obra'</tr>
-                    <th> Writers </th>
-                    <th> Artists </th>
-                    <th> ISWC </th>
-                    <th> Type </th>
-                    <th> Lyrics Languages </th>
-                    <th> Attributes </th>
-                    <th> Ratting </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr> -- RAW
-                    <td> --COLUMN
-                        <bdi> Tom Morello </bdi> -- Real Name
-                    </td>
-                </tr>
-            </tbody>
-    """
-    # (lyricist, composer) ; (writer)
-    # for tr in html_soup.find_all(table', class_='tbl'):
+
+# dfs = pd.read_html(str(table))
+# print(dfs)
+
+#
 
 
-    data = pd.read_html(html_soup, header=0)
-    print(data[0])
+"""
+        table_work class="tbl">
+        <thead>
+            <tr>
+                <th> Work 'Obra'</tr>
+                <th> Writers </th>
+                <th> Artists </th>
+                <th> ISWC </th>
+                <th> Type </th>
+                <th> Lyrics Languages </th>
+                <th> Attributes </th>
+                <th> Ratting </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr> -- RAW
+                <td> --COLUMN
+                    <bdi> Tom Morello </bdi> -- Real Name
+                </td>
+            </tr>
+        </tbody>
+"""
+# (lyricist, composer) ; (writer)
+# for tr in html_soup.find_all(table', class_='tbl'):
 
-    # table_work = html_soup.find('table', {'class': 'tbl'})
-    # print(table_work)
+# table_work = html_soup.find('table', {'class': 'tbl'})
+# print(table_work)
 
+# data = pd.read_html(html_soup, header=0)
+# print(data)
 
-    # for wr in tr.find_all('td'):
-    # print(wr)
-    #     if 'writer' in str(wr):
-    #         print(wr)
+# for wr in tr.find_all('td'):
+# print(wr)
+#     if 'writer' in str(wr):
+#         print(wr)
 
 
 # table_div = html_soup.find_all(table_work', class_='tbl')
